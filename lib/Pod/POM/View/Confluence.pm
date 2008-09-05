e5a8b1230edd6e8dd078469749459dfac60d9494
Philippe Bruhat (BooK) <book@cpan.org>
2008-09-05 18:06:04 +0200

    starter directories and files from module-starter
        module-starter --module Pod::POM::View::Confluence --eumm --mb --author "Philippe Bruhat (BooK)" --email book@cpan.org

---
+++ lib/Pod/POM/View/Confluence.pm
@@ -0,0 +1,107 @@
+package Pod::POM::View::Confluence;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Pod::POM::View::Confluence - The great new Pod::POM::View::Confluence!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Pod::POM::View::Confluence;
+
+    my $foo = Pod::POM::View::Confluence->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Philippe Bruhat (BooK), C<< <book at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-pod-pom-view-confluence at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Pod-POM-View-Confluence>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Pod::POM::View::Confluence
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Pod-POM-View-Confluence>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Pod-POM-View-Confluence>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Pod-POM-View-Confluence>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Pod-POM-View-Confluence>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Philippe Bruhat (BooK), all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Pod::POM::View::Confluence
